54f799c077501a8589d1f79c5d2297686fdcf03a
oldenm <85153052+oldenm@users.noreply.github.com>
2022-11-26 12:44:34 +0300

    TelBot

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5806413557:AAGBx6oL2QhhZzw8_UxDqjKv3V3QoAgo-go'
+keys = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'юань': 'CNY',
+    'йена': 'JPY',
+
+}
---
+++ main.py
@@ -0,0 +1,43 @@
+import telebot
+from config import keys, TOKEN
+from extensions import ConvertionException, ValueConvecter
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \' \
+<в какую валюту перевести> \
+<колличесво переводимой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров.')
+        quote, base, amount = values
+        if float(amount) <= 0:
+            raise ConvertionException(f'Количество отрицательное или равно нулю\n{amount}')
+        total_base = ValueConvecter.get_price(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Неудалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base * float(amount)}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling(none_stop=True)
